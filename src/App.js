9de2c6426bd6eded50ae830e83be0a0ebdb2cdcd
shyamal <shyamalsha@gmail.com>
2020-12-22 15:12:06 -0500

    Added fetch methode to get data from API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 import Cardlist from "./Cardlist";
 import SearchBox from './SearchBox';
-import { robots } from "./robots";
 
 class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield:''
         }
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots:users}));
     }
     onSearchChange = (event) => {
         this.setState({searchfield:event.target.value.toLowerCase()});
@@ -18,13 +22,17 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield)
         });
-        return (
-            <div className="tc">
-                <h1>Robofriends</h1>
-                <SearchBox onSearchChange={this.onSearchChange}></SearchBox>
-                <Cardlist robots={filteredRobots} /> 
-            </div>
-        )
+        if(this.state.robots.length === 0){
+            return <div className="tc"><h1>Loading...</h1></div>
+        }else{
+            return (
+                <div className="tc">
+                    <h1>Robofriends</h1>
+                    <SearchBox onSearchChange={this.onSearchChange}></SearchBox>
+                    <Cardlist robots={filteredRobots} /> 
+                </div>
+            );
+        }
     }
 }
 export default App;
